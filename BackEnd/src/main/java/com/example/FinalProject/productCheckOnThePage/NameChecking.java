d0dacaeca908c0ba8737840e880e54008e12105f
DigiDoggy <aituganov.ilja@gmail.com>
2023-07-20 13:42:05 +0300

    test

--- BackEnd/src/main/java/com/example/FinalProject/productCheckOnThePage/NameChecking.java
+++ BackEnd/src/main/java/com/example/FinalProject/productCheckOnThePage/NameChecking.java
@@ -12,17 +12,8 @@ public class NameChecking {
 
     //check what names of product we have on the page, if in collection list of Products from the page don`t contains
     // similar name like in DB, then remove
+    // todo look
     public static void checkingName(List<Product> onThePage, List<String> groceriesName) {
-
-        int index = 0;
-
-        for (Product product : onThePage) {
-            boolean contains = ProductFilter.containsAllWords(product.getName(), groceriesName.get(index));
-
-            if (!contains) {
-                onThePage.remove(product);
-            }
-        }
 
     }
 
