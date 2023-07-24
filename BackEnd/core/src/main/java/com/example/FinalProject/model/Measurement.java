f2e8e42550d231cf49990b2acb633dee6951be55
Vladimir Pavlovskii <antivoland@gmail.com>
2023-07-24 16:40:00 +0300

    move model to core module

--- BackEnd/core/src/main/java/com/example/FinalProject/model/Measurement.java
+++ BackEnd/core/src/main/java/com/example/FinalProject/model/Measurement.java
@@ -1,8 +1,5 @@
 package com.example.FinalProject.model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
